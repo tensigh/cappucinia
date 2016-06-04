c55de3a2245a276a443426a70537dee4bd0071ab
tensigh <kurisu@linuxdiaries.com>
2016-06-04 08:38:17 +0000

    Restored original migration

--- db/migrate/20160424140555_create_cappucinia.rb
+++ db/migrate/20160424140555_create_cappucinia.rb
@@ -3,14 +3,6 @@ class CreateCappucinia < ActiveRecord::Migration
     create_table :cappucinia do |t|
       t.string :name
       t.string :body
-      t.string :restaurantName
-      t.string :restaurantAdd
-      t.string :restaurantPhone
-      t.string :restaurantWebsite
-      t.string :productName
-      t.string :productPrice
-      t.string :productRating
-      t.string :comments
 
       t.timestamps null: false
     end
